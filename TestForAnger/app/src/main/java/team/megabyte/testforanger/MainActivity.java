63440403b02e1ae08339cc13f2426af53bdecfb5
dmelechow <melehow-dmitriy@mail.ru>
2016-02-07 03:12:55 +0300

    advert

--- TestForAnger/app/src/main/java/team/megabyte/testforanger/MainActivity.java
+++ TestForAnger/app/src/main/java/team/megabyte/testforanger/MainActivity.java
@@ -2,6 +2,8 @@ package team.megabyte.testforanger;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.appodeal.ads.Appodeal;
 import com.crashlytics.android.Crashlytics;
 import com.crashlytics.android.ndk.CrashlyticsNdk;
 import io.fabric.sdk.android.Fabric;
@@ -9,10 +11,21 @@ import io.fabric.sdk.android.Fabric;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        Appodeal.onResume(this, Appodeal.BANNER);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics(), new CrashlyticsNdk());
         setContentView(R.layout.activity_main);
+
+
+        String appKey = "698c2d3ae72e92560d4e62cb5d6a9503fb1687428bafeaf6";
+        Appodeal.initialize(this, appKey, Appodeal.INTERSTITIAL | Appodeal.BANNER);
+        Appodeal.show(this, Appodeal.INTERSTITIAL);
 
         getFragmentManager()
                 .beginTransaction()
